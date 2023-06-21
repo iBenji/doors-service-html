8c215ab5bbe27e904eab0cc4e5bef7570b074eab
deprussian <dmitrii.tsikh@gmail.com>
2023-06-21 20:07:13 +0500

    Update 1.0
    Added Adaptive (255 - 1044)
    Fixed Doors catalog (incorrect sizing)
    Added picture tag for img
    Added Burger menu

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,5 @@
 const headerPhoto = document.querySelector('.header__top');
-const photos = ['../img/jpg/hhphoto1.jpg', '../img/jpg/hhphoto2.jpg']; // Здесь указывайте пути к фотографиям
+const photos = ['./img/jpg/hhphoto1.jpg', './img/jpg/hhphoto2.jpg']; // Здесь указывайте пути к фотографиям
 
 var currentPhotoIndex = 0;
 
@@ -29,3 +29,22 @@ var swiper = new Swiper(".mySwiper", {
       clickable: true,
     },
   });
+
+// Бургер меню
+var burgerMenu = document.querySelector(".header__burger-menu");
+var headerList = document.querySelector(".header__list");
+
+burgerMenu.addEventListener("click", function(event) {
+  event.stopPropagation(); // Остановим всплытие события, чтобы бургер-меню не закрывалось сразу после клика на само меню
+  
+  headerList.classList.toggle("active");
+});
+
+document.addEventListener("click", function(event) {
+  var targetElement = event.target;
+  var isClickInside = headerList.contains(targetElement);
+  
+  if (!isClickInside) {
+    headerList.classList.remove("active");
+  }
+});
